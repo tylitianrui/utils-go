393007f0b21188ffa180aa922e0632edb9998958
tyltr <tylitianrui@126.com>
2019-12-27 07:21:27 +0800

    生成随机字符串

--- random.go
+++ random.go
@@ -6,14 +6,62 @@ import (
 	"time"
 )
 
+type RandKind int
+
+const (
+	LetterAll RandKind = iota
+	LetterUpper
+	LetterLower
+)
+const (
+	NumLetter  = 26
+	UpperStart = 65
+	LowerStart = 97
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 // a random  number in [start,end)
 // if  start >= end ,return  error
 func RandInt(start, end int) (int, error) {
 	if start >= end {
 		return 0, errors.New("start should less than end")
 	}
-	seed := time.Now().UnixNano()
 	r := end - start
-	rand.Seed(seed)
 	return rand.Intn(r) + start, nil
+}
+
+//
+func RandLetter(len int, kind RandKind) string {
+	b := make([]byte, len)
+	var (
+		s int
+		l int
+	)
+	switch kind {
+	case LetterLower:
+		s = LowerStart
+		l = NumLetter
+
+	case UpperStart:
+		s = UpperStart
+		l = NumLetter
+	default:
+		s = UpperStart
+		l = NumLetter * 2
+
+	}
+	for i := 0; i < len; i++ {
+		idx := rand.Intn(l)
+
+		if idx >= NumLetter {
+			idx += 7
+		}
+
+		b[i] = byte(idx + s)
+	}
+
+	return string(b)
 }
--- random_test.go
+++ random_test.go
@@ -2,6 +2,10 @@ package utils
 
 import "testing"
 
+func TestRandLetter(t *testing.T) {
+	t1 := RandLetter(12, LetterAll)
+	t.Log(t1)
+}
 func TestRandInt(t *testing.T) {
 	for i := 0; i < 100; i++ {
 		r, _ := RandInt(-12, 12)
@@ -14,3 +18,15 @@ func TestRandInt(t *testing.T) {
 	}
 
 }
+
+//func TestCreateRandomString(t *testing.T) {
+//	for i:=0; i < 100; i++ {
+//		l:= RandUpperLetter(12)
+//		if  len(l)==12{
+//			t.Log(l)
+//		}else {
+//			t.Fail()
+//		}
+//	}
+//
+//}
