7b8ef1226d7c01d85c20759de3df78a56bf8cd7b
tyltr <tianrui_li@qingsongchou.com>
2020-01-14 14:06:30 +0800

    获取目录下的文件，不包括目录

--- file.go
+++ file.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
@@ -9,4 +10,36 @@ import (
 func CurrentPath() string {
 	path, _ := filepath.Abs(os.Args[0])
 	return path
+}
+
+// 指定的路径是否存在
+// false  不存在;true 存在
+func Exist(path string) (bool, error) {
+	var err error
+
+	if _, err = os.Stat(path); err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+// 获取目录下的文件，不包括目录
+func SubFileName(path string) ([]string, error) {
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+	list := make([]string, len(files))
+	// 获取文件，并输出它们的名字
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		list = append(list, file.Name())
+	}
+	return list, nil
 }
--- file_test.go
+++ file_test.go
@@ -1,7 +1,19 @@
 package utils
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
 func TestCurrentPath(t *testing.T) {
 	t.Log(CurrentPath())
+}
+
+func TestSubFileName(t *testing.T) {
+	l, err := SubFileName("./")
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println(len(l))
+	}
 }
