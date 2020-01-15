c3a5611b6a9465ff15ec50c0634c16e416bb9f25
tyltr <tylitianrui@126.com>
2020-01-15 23:26:15 +0800

    新增sha1

--- encryt.go
+++ encryt.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
 	"encoding/base64"
 	"encoding/hex"
 )
@@ -9,6 +10,13 @@ import (
 // return  md5 checksum value
 func MD5(b []byte) string {
 	checksum := md5.Sum(b)
+	return hex.EncodeToString(checksum[:])
+
+}
+
+// return  sha1 checksum value
+func SHA1(b []byte) string {
+	checksum := sha1.Sum(b)
 	return hex.EncodeToString(checksum[:])
 
 }
