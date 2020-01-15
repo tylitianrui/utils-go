b904a7332982766fd386f9ba00a0e442462cb9f4
tyltr <tianrui_li@qingsongchou.com>
2020-01-15 11:13:00 +0800

    大进制字符串表示int，可用于压缩字符串

---
+++ other.go
@@ -0,0 +1,22 @@
+package utils
+
+import "fmt"
+
+var (
+	NumBase = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+)
+
+func Convert10toN(input, n int) string {
+	if n <= 1 {
+		panic("param  error")
+	}
+	if n > len(NumBase) {
+		panic("超出表示范围，请修改NumBase或者参数n")
+	}
+	if input < n {
+		return fmt.Sprintf("%s", NumBase[input])
+	}
+	return fmt.Sprintf("%s%s", Convert10toN(input/n, n), NumBase[input%n])
+}
