22b90686c9aeed9f2979927a3202cdc452440464
tyltr <tylitianrui@126.com>
2020-02-23 13:07:35 +0800

    新增sha512

--- encryt.go
+++ encryt.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/base64"
 	"encoding/hex"
 )
@@ -23,8 +24,15 @@ func SHA1(b []byte) string {
 }
 
 // return  sha256 checksum value
-func Hmac(b []byte) string {
+func SHA256(b []byte) string {
 	checksum := sha256.Sum256(b)
+	return hex.EncodeToString(checksum[:])
+
+}
+
+// return  sha512 checksum value
+func SHA512(b []byte) string {
+	checksum := sha512.Sum512(b)
 	return hex.EncodeToString(checksum[:])
 
 }
